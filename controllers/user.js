d5b1c058863b65ba7a0004460d915466da73cd42
camontes <pope624@hotmail.com>
2025-06-11 09:04:01 -0500

    user create endpoint

---
+++ controllers/user.js
@@ -0,0 +1,12 @@
+
+
+const create = (req, res) =>{
+
+    res.status(200).json({
+        message:"OK"
+    });
+}
+
+module.exports = {
+    create
+}
--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 const connectDB = require("./Db/connection");
-
+const express = require('express');
+const cors = require('cors');
+const userRoutes = require("./routes/user");
 
 connectDB();
+
+ // Create an Express application
+ const app = express();
+ const PORT = process.env.PORT || 3000;
+
+  // Middleware
+app.use(cors());
+
+// Parse JSON bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+///Use Routes
+app.use('/api', userRoutes);
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
